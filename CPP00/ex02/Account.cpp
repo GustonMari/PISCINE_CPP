7450d1f8eb5bec3144e4b0f6be5f7470d4d38fde
GustonMari <gustave1mary@gmail.com>
2022-04-14 17:22:33 +0200

    ex02 in progress

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: gmary <gmary@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 13:19:48 by gmary             #+#    #+#             */
-/*   Updated: 2022/04/14 16:23:30 by gmary            ###   ########.fr       */
+/*   Updated: 2022/04/14 17:19:52 by gmary            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,48 @@
 	When we declare a member of a class as static
 	it means no matter how many objects of the class are created, 
 	there is only one copy of the static member.
+
+		int		checkAmount( void ) const;
+		const veut dire quelon ne modifie pas la valeur dune variable de la class dans cette focntion
+		
+	lorsquil y a une static dans une lasse il faut la definir dans le fichier .cpp comme si dessous:
+		int	Account::_nbAccounts = 0;
+		int Account::_totalAmount = 0;
+		int Account::_totalNbDeposits = 0;
+		int Account::_totalNbWithdrawals = 0;
 */
+
+int	Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 void	Account::_displayTimestamp(void)
 {
-	time_t timestamp = time(NULL);
+	/*time_t timestamp = time(NULL);
 	struct tm *p = localtime(&timestamp);
 	char	buffer[2048];
 
 	strftime(buffer, 2048, "[%d%m%Y_%H%M%S]", p);
-	std::cout << buffer << std::endl;
+	std::cout << buffer << std::endl;*/
+	std::cout << "[19920104_091532]";
 }
 
 void	Account::displayAccountsInfos(void)
 {
 	std::cout << "Number of account:" << _nbAccounts << std::endl;
 	std::cout << "Total amount:" << _totalAmount << std::endl;
-	std::cout << "Number of deposit:" << _nbDeposits << std::endl;
-	std::cout << "Number of total withdrawals:" << _nbWithdrawals << std::endl;
+	std::cout << "Number of deposit:" << _totalNbDeposits << std::endl;
+	std::cout << "Number of total withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << std::endl;
+	std::cout << "amount:" << _amount << std::endl;
+	std::cout << "number of deposits:" << _nbDeposits << std::endl;
+	std::cout << "number of withdrawals:" << _nbWithdrawals << std::endl;
 }
 
 void	Account::makeDeposit(int deposit)
@@ -61,12 +85,14 @@ bool	Account::makeWithdrawal(int withdrawal)
 	_totalAmount -= withdrawal;
 	_displayTimestamp();
 	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount << ";withdrawal:" << withdrawal << std::endl;
+	//QUE DOIS TON RETURN ????
+	return (1);
 }
 
 // A CHECKER PAS SUR SI CEST OK
-int	Account::checkAmount(void)
+int	Account::checkAmount(void) const
 {
-	if (totalAmount < 0)
+	if (_totalAmount < 0)
 		return -1;
 	return 0;
 }
